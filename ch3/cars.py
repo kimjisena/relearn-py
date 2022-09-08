0f6d4250947aa5ccca4c5798f22e4d209fe4f340
Kim Jisena <pjohmaths@gmail.com>
2022-09-08 15:40:38 +0300

    lists: sorting, sorting-in-place and reversing

---
+++ ch3/cars.py
@@ -0,0 +1,19 @@
+#! /bin/python3
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+print("\nHere is the reversed list:")
+cars.reverse()
+print(cars)
+
+print(f"\nI got {len(cars)} cars.")
+
