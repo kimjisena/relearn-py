c4795b46d57f24c12634c5ffc30ab40fa67a05ea
Kim Jisena <pjohmaths@gmail.com>
2022-09-09 01:22:44 +0300

    if: working with lists

--- ch5/toppings.py
+++ ch5/toppings.py
@@ -1,13 +1,16 @@
 #! /bin/python3
 
-requested_toppings = ['mushrooms', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers', 
+                      'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms','french fries', 'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
-
-print("\nFinished making your pizza!")
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping not in available_toppings:
+            print(f"Sorry, we don't have {requested_topping}.")
+        else:
+            print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
 
