2f1b75678b3fea5b5b5b5f246f03c54d403e9026
Kim Jisena <pjohmaths@gmail.com>
2022-09-09 22:01:11 +0300

    function: modules and imports

---
+++ ch8/making_pizzas.py
@@ -0,0 +1,11 @@
+#! /bin/python3
+
+# import pizza
+# from pizza import make_pizza
+# from pizza import make_pizza as mp
+# import pizza as p
+from pizza import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
