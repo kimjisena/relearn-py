44dc38e64237cb3b25cb203fe2f1c5394366e3f8
Kim Jisena <pjohmaths@gmail.com>
2022-09-10 00:29:44 +0300

    class: importing a single class

--- ch9/car.py
+++ ch9/car.py
@@ -1,4 +1,5 @@
 #! /bin/python3
+"""A set of classes used to represent gas and electric cars."""
 
 class Car:
     """A simple attempt to represent a car."""
@@ -33,12 +34,34 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
 
